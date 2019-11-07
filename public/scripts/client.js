474cc2350b6f03e20e4efa003abe8897882a4675
Amy Bemister <amy.bemister@gmail.com>
2019-11-07 00:53:50 +0000

    Prevented XSS with escaping

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function(tweets) {
   $('.tweets-container').empty();
 // loops through tweets
@@ -41,7 +15,15 @@ for (let tweet of tweets) {
 }
 }
 
+
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
 const createTweetElement = function(tweet) {
+  
 let $tweet = `
   <section class="tweet-container">
     <article class="tweet">
@@ -52,7 +34,7 @@ let $tweet = `
         </div>
         <div class="tweeter-handle">${tweet.user.handle}</div>
       </header>
-      <section class="tweeter">${tweet.content.text}</section>
+      <section class="tweeter">${escape(tweet.content.text)}</section>
       <footer>
         <div class="date-posted">${tweet.created_at}</div>
       </footer>
@@ -67,11 +49,11 @@ return $tweet;
 
     const formData = $(this).serialize();
 
-    $.post('/tweets/', formData, function() {
+    $.post('/tweets/', formData, function(tweet) {
       console.log("done posting");
 
       $.getJSON('/tweets/', function(updatedTweets) {
-        console.log("ll tweetssss", updatedTweets)
+        console.log("all tweetssss", updatedTweets)
         renderTweets(updatedTweets);
       })
     })
