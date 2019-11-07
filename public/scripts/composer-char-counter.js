d8a52da62ee9bb1e3ccaad1c3a0cb1095008a2d1
Amy Bemister <amy.bemister@gmail.com>
2019-11-07 20:16:30 +0000

    Added jquery to slide down error message when counter exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
 $('#tweet-input').keyup(function () {
   let count = 140;
   let length = $(this).val().length;
-  if (length > 140) {
+  if (length >= 140) {
     $('span.counter').css('color', 'red');
+    $('.error1').slideDown("slow");
+  } else if (length < 140) {
+    $('.error1').slideUp("slow");
   }
   let remainingChar = count - length;
   $('span.counter').text(remainingChar);
