3121bf8420945823212bbc9ad9c5750cfff50991
Amy Bemister <amy.bemister@gmail.com>
2019-11-07 23:07:37 +0000

    Updated to support responsive design

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 $('#tweet-input').keyup(function () {
   let count = 140;
   let length = $(this).val().length;
-  if (length > 140) {
+  if (length >= 140) {
     $('span.counter').css('color', 'red');
     $('.error1').slideDown("slow");
   } else if (length < 140) {
